78f576415fe0ad3517810cfd0e04c654b6e51b46
Daniel Imhoff <dwieeb@gmail.com>
2018-11-06 16:32:26 -0600

    some test

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,23 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+document.addEventListener('deviceready', () => {
+  console.log('deviceready received');
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+  // setTimeout(() => {
+  //   console.log('doing request');
+  //   doRequest();
+  // }, 1500);
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+  // setTimeout(() => {
+  //   console.log('starting worker');
+  //   const worker = new Worker('js/worker.js');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+  //   worker.postMessage(['what', 'up', 'fren']);
+  //   worker.onmessage = (e) => {
+  //     console.log('wow I got a message from worker:', e);
+  //   };
+  // }, 3000);
+});
 
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
+async function doRequest() {
+  const res = await fetch('http://10.0.1.237:3000', { method: 'POST', mode: 'cors', body: 'hello there' });
+  console.log(res);
+}
---
+++ www/js/worker.js
@@ -0,0 +1,15 @@
+onmessage = function(e) {
+    console.log('worker: hello I got a message');
+    console.log('worker: and here it is:', e);
+    console.log('now I will do work');
+
+    setTimeout(() => {
+        const req = new XMLHttpRequest();
+        req.addEventListener('load', function () {
+            console.log('worker: loaded a thing', this.responseText);
+            postMessage('thanks');
+        });
+        req.open('POST', 'http://10.0.1.237:3000');
+        req.send('here is some datar');
+    }, 500);
+}
