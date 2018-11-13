e4765e620dc354fcfaa8834e057e0d5e93c8badf
jcesarmobile <jcesarmobile@gmail.com>
2018-11-13 20:02:28 +0100

    Added some fixes and new tests

--- www/js/fetch.js
+++ www/js/fetch.js
@@ -10,17 +10,57 @@ document.addEventListener('deviceready', () => {
     event.preventDefault();
     sendPOST();
   });
+
+  document.getElementById('send-get-cors').addEventListener('click', event => {
+    event.preventDefault();
+    sendGETCORS();
+  });
+
+  document.getElementById('send-post-cors').addEventListener('click', event => {
+    event.preventDefault();
+    sendPOSTCORS();
+  });
+
+  document.getElementById('send-get-local').addEventListener('click', event => {
+    event.preventDefault();
+    sendGETLocal();
+  });
 });
 
 async function sendGET() {
   console.log('performing GET');
-  const res = await fetch('http://10.0.1.237:3000');
-  console.log('response:', res);
+  doGET('http://webviewtests.000webhostapp.com/nocors/heroes.json');
+}
+
+async function sendGETCORS() {
+  console.log('performing GET with CORS enabled');
+  doGET('http://webviewtests.000webhostapp.com/cors/heroes.json');
+}
+
+async function sendGETLocal() {
+  console.log('performing GET to local file');
+  doGET('./heroes.json');
+}
+
+async function doGET(url) {
+  const res = await fetch(url);
+  const data = await res.json()
+  console.log('response:', data);
 }
 
 async function sendPOST() {
   console.log('performing POST');
-  const res = await fetch('http://10.0.1.237:3000', {
+  doPOST('http://webviewtests.000webhostapp.com/nocors/post.php');
+}
+
+
+async function sendPOSTCORS() {
+  console.log('performing POST with CORS enabled');
+  doPOST('http://webviewtests.000webhostapp.com/cors/post.php');
+}
+
+async function doPOST(url) {
+  const res = await fetch(url, {
     method: 'POST',
     mode: 'cors',
     body: 'hello from app',
@@ -28,6 +68,7 @@ async function sendPOST() {
       'Content-Type': 'text/plain; charset=utf-8',
     },
   });
-
-  console.log('response:', res);
+  const data = await res.text();
+  console.log('response:', data);
 }
+
